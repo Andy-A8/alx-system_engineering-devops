9f7e27816c50aab7051fe5b2123fd6013f921ab8
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-05-21 13:12:59 -0700

    Updated T-0 solution

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Returns information about the to-do list for a given employee ID."""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        url = "https://jsonplaceholder.typicode.com/"
+        user_resp = requests.get(url + 'users/{}'.format(sys.argv[1]))
+        todo_resp = requests.get(url + 'todos?userId={}'.format(sys.argv[1]))
+        if all([user_resp, todo_resp]) is not None:
+            try:
+                user_d = json.loads(user_resp.text)
+            except JSONDecodeError:
+                user_d = {}
+            try:
+                todo_l = json.loads(todo_resp.text)
+            except JSONDecodeError:
+                todo_l = []
+            tasks = ''
+            completed = 0
+            total = 0
+            if type(todo_l) is list:
+                for todo in todo_l:
+                    if type(todo) is not dict:
+                        continue
+                    if todo.get('completed') is True:
+                        tasks += '\t {}\n'.format(todo.get('title'))
+                        completed += 1
+                total = len(todo_l)
+            if type(user_d) is dict and user_d != {}:
+                emp_Id = 'Employee {} is done with tasks({}/{}):\n'\
+                    .format(user_d.get('name'), completed, total)
+                print(emp_Id + tasks, end='')
