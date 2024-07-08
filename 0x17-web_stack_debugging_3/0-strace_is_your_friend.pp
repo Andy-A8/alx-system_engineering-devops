11ba1ebf61507beb1c80ac3257594411afd261b5
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-07-07 17:08:50 -0700

    Updated repository

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# A puppet script to fix a server error in Wordpress file 'wp-settings.php'.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => 'usr/local/bin/:/bin/'
-}
