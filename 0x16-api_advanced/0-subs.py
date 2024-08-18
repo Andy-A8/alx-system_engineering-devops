a7f401a9ad09e879a1cc99e7b95bff026baef477
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-08-17 18:26:45 -0700

    Initial upload of solution to T_0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Retuns the number of total subscribers for a given subreddit"""
-    url = "https://www.reddit.com/dev/api/{}/about.json".format(subreddit)
-    user_agent = {'User_agent': 'Google Chrome Version 127.0.6533.100'}
-    response = requests.get(url, headers=user_agent, allow_redirects=False)
-
-    if response.status_code == 404:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User_Agent': 'Chrome/127.0.6533.100'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    results = response.json()
-    return results.get("data").get("subscribers")
